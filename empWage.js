2ef3ea6362292ff6eebdfc664f7f77e0a413bf1d
s8189882 <s8189882@gmail.com>
2022-10-11 19:46:16 +0530

    Added UC1 if-else statement

--- empWage.js
+++ empWage.js
@@ -1,1 +1,9 @@
 console.log("Welcome To Employee Wage Computation Application!");
+
+const IS_ABSENT = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if (empCheck == IS_ABSENT) {
+        console.log("\nEmployee is Absent.");
+    } else {
+        console.log("\nEmployee is Present.");
+    }
