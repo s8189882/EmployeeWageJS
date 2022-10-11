bae79c4d418c18248c434feff2d4a421700271ac
s8189882 <s8189882@gmail.com>
2022-10-11 20:04:59 +0530

    Added UC2 switch case statemnet

--- empWage.js
+++ empWage.js
@@ -1,9 +1,29 @@
 console.log("Welcome To Employee Wage Computation Application!");
 
-const IS_ABSENT = 0;
-    let empCheck = Math.floor(Math.random() * 10) % 2;
-    if (empCheck == IS_ABSENT) {
-        console.log("\nEmployee is Absent.");
-    } else {
-        console.log("\nEmployee is Present.");
-    }
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+
+let empHours = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+switch (empCheck) {
+    case IS_PART_TIME :
+        empHours = PART_TIME_HOURS;
+        console.log("\nEmployee is Present part-time.")
+        break;
+
+    case IS_FULL_TIME :
+        empHours = FULL_TIME_HOURS;
+        console.log("\nEmployee is Present full-time.")
+        break;
+        
+    default :
+        empHours = 0;
+        console.log("\nEmployee is Absent.\nExiting the program!");
+}
+
+let empWage = empHours * WAGE_PER_HOUR;
+console.log("Employee Wage : " + empWage);
