6913a363db149ec0207e14684223d185967da5e5
s8189882 <s8189882@gmail.com>
2022-10-11 20:22:12 +0530

    Added UC3 functions in javascript

--- empWage.js
+++ empWage.js
@@ -6,24 +6,23 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
+function getWorkingHours (empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME :
+            console.log("\nEmployee is Present part-time.")
+            return PART_TIME_HOURS;
 
-let empHours = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case IS_PART_TIME :
-        empHours = PART_TIME_HOURS;
-        console.log("\nEmployee is Present part-time.")
-        break;
-
-    case IS_FULL_TIME :
-        empHours = FULL_TIME_HOURS;
-        console.log("\nEmployee is Present full-time.")
-        break;
-        
-    default :
-        empHours = 0;
-        console.log("\nEmployee is Absent.\nExiting the program!");
+        case IS_FULL_TIME :
+            console.log("\nEmployee is Present full-time.")
+            return FULL_TIME_HOURS;
+            
+        default :
+            console.log("\nEmployee is Absent.\nExiting the program!");
+            return 0;
+    }
 }
 
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHours = getWorkingHours(empCheck);
 let empWage = empHours * WAGE_PER_HOUR;
 console.log("Employee Wage : " + empWage);
