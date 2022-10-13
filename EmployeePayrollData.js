ebf516b5eea0ffbabd7fbcafe81be3da2cc7968d
s8189882 <s8189882@gmail.com>
2022-10-13 16:24:46 +0530

    Added UC12 extended to store gender and start date

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,11 +1,15 @@
 class EmployeePayrollData {
     id;
     salary;
+    gender;
+    startDate;
 
-    constructor(id, name, salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     get name() {
@@ -17,7 +21,9 @@ class EmployeePayrollData {
     }
 
     toString() {
-        return "ID : " + this.id + " Name : " + this.name + " Salary : " + this.salary;
+        const options = { year : 'numeric', month : 'long', day : 'numeric'};
+        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "ID : " + this.id + " Name : " + this.name + " Salary : " + this.salary + " Gender : " + this.gender + "Start Date : " + empDate;
     }
 }
 
@@ -25,3 +31,5 @@ let employeePayrollData = new EmployeePayrollData(111, "Mark", 50000000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "John";
 console.log(employeePayrollData.toString());
+let newEmployeePayrollData = new EmployeePayrollData(222, "Teresa", 30000000, 'F', new Date());
+console.log(newEmployeePayrollData.toString());
