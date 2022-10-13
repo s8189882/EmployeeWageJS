ee79354403d65f46324abc92a52e6b42ea7e53de
s8189882 <s8189882@gmail.com>
2022-10-13 16:52:37 +0530

    Added UC14 pattern checking for all fields

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -12,15 +12,51 @@ class EmployeePayrollData {
         this.startDate = params[4];
     }
 
+    get id() {
+        return this._id;
+    }
+
+    set id(id) {
+        let idRegex = RegExp('^[1-9][0-9]*$');
+        if (idRegex.test(id))
+            this._id = id;
+        else
+            throw "ID input is Invalid!";
+    }
+
     get name() {
-        return this.name;
+        return this._name;
     }
 
     set name(name) {
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
         if (nameRegex.test(name))
             this.name = name;
-        else throw 'Name is Invalid!';
+        else throw 'Name input is Invalid!';
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        let salaryRegex = RegExp('^[1-9][0-9]*$');
+        if (salaryRegex.test(salary))
+            this._salary = salary;
+        else
+            throw "Salary input is Invalid!";
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        let genderRegex = RegExp('^[F|f|M|m]$');
+        if (genderRegex.test(gender))
+            this._gender = gender;
+        else 
+            throw "Gender input is Invalid!";
     }
 
     toString() {
